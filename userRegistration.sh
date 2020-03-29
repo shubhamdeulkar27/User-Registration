ac44b082596d0c0500258c9f202b6461e2a45118
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-29 22:31:04 +0530

    Refactored: Validating Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 
-#TAKING INPUT
+#TAKING INPUTS
 read -p "Enter First Name :  " firstName
+read -p "Enter Last Name : " lastName
 
-patternFirstName="([A-Z]{1}[a-z]{2})"
+function checkName(){
+	patternName="([A-Z]{1}[a-z]{2})"
+	if [[ $1 =~ $patternName ]] && [[ $2 =~ $patternName ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
 
-if [[ $firstName =~ $patternFirstName ]]
-then
-	echo "Valid"
-else
-	echo "Invalid"
-fi
+checkName $firstName $lastName
